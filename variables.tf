1fca6085e43f98f0856445e0c970ae1a192978a3
Oshi Gupta <47573417+oshi36@users.noreply.github.com>
2024-01-09 05:28:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,7 @@ variable "ami" {
   type = map(string)
   default = {
     master = "ami-00d8834ea9c9ecd09"
-<<<<<<< HEAD
-=======
     worker = "ami-00d8834ea9c9ecd09"
->>>>>>> 456cddac11ca2cb947317261adb90f308161ae70
   }
 }
 
@@ -17,15 +14,11 @@ variable "instance_type" {
   type = map(string)
   default = {
     master = "t2.medium"
-<<<<<<< HEAD
-  }
-=======
     worker = "t2.micro"
   }
 }
 
 variable "worker_instance_count" {
   type    = number
-  default = 1
->>>>>>> 456cddac11ca2cb947317261adb90f308161ae70
+  default = 0         # increase if worker nodes needs to be created 
 }
