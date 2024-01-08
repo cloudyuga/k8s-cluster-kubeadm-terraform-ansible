ada37636201e312246071c8471ec7cfcf75b9db8
root <oshiagupta36@gmail.com>
2024-01-08 09:47:50 +0000

    added terraform scripts to create single kubeadm with nginx ingress

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Launch master node
+resource "aws_instance" "k8s_master" {
+  ami           = var.ami["master"]
+  instance_type = var.instance_type["master"]
+  tags = {
+    Name = "k8s-master"
+  }
+  key_name        = aws_key_pair.k8s.key_name
+  security_groups = ["k8s_master_sg"]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("k8s")
+    host        = self.public_ip
+  }
+  provisioner "file" {
+    source      = "./deploy.yaml"
+    destination = "/home/ubuntu/deploy.yaml"
+  }
+  provisioner "file" {
+    source      = "./master.sh"
+    destination = "/home/ubuntu/master.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/master.sh",
+      "sudo sh /home/ubuntu/master.sh k8s-master"
+    ]
+  }
+  
+}
